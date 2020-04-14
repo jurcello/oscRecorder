c291c4b3c82ec897468e5c6e4782a183fe5cf2b4
Jur de Vries <info@jurdevries.nl>
2020-04-14 16:02:38 +0200

    Added a basic track player.

---
+++ src/TrackPlayer.h
@@ -0,0 +1,50 @@
+//
+// Created by Jur de Vries on 14/04/2020.
+//
+
+#ifndef OSCRECORDER_TRACKPLAYER_H
+#define OSCRECORDER_TRACKPLAYER_H
+
+#include "Track.h"
+#include <exception>
+
+class noNewMessageException: public std::exception
+{} noNew;
+
+template <typename MessageType>
+class TrackPlayer {
+public:
+    TrackPlayer(Track<MessageType> &track)
+    :track(track), currentPosition(0)
+    {}
+
+    MessageType getNextMessage(uint64_t time) {
+        if (! this->hasMessages(time)) {
+            throw noNew;
+        }
+        return track.getEventAt(currentPosition++).message;
+    }
+
+    bool hasMessages(uint64_t time) {
+        return currentPosition < track.size() && track.getEventAt(currentPosition).millis <= time;
+    }
+
+    void seek(u_int64_t time) {
+        currentPosition = track.findIndexAtTime(time);
+    }
+
+    void rewind() {
+        currentPosition = 0;
+    }
+
+    uint64_t timeLength() {
+        return track.getEventAt(track.size() - 1).millis;
+    }
+
+private:
+    Track<MessageType> &track;
+    unsigned long currentPosition;
+};
+
+
+#endif //OSCRECORDER_TRACKPLAYER_H
---
+++ tests/test-track-player.cpp
@@ -0,0 +1,86 @@
+#include "catch.hh"
+#include "Track.h"
+#include "TrackPlayer.h"
+
+typedef Track<float> FloatTrack;
+typedef TrackEvent<float> FloatEvent;
+typedef TrackPlayer<float> FloatPlayer;
+
+TEST_CASE("A track player has a method getNextMessage which returns the messages of the track one at a time", "[TrackPlayer]") {
+    FloatTrack track;
+    FloatPlayer player(track);
+    for (uint64_t i = 0; i < 5; i++) {
+        track.addEvent(FloatEvent(i, .1f*i));
+    }
+    REQUIRE(player.getNextMessage(10) == 0.f);
+    REQUIRE(player.getNextMessage(10) == .1f);
+    REQUIRE(player.getNextMessage(10) == .2f);
+    REQUIRE(player.getNextMessage(10) == .3f);
+    REQUIRE(player.getNextMessage(10) == .4f);
+}
+
+TEST_CASE("The track player throws a runtime exception when there are no next messages at the end", "[TrackPlayer]") {
+    FloatTrack track;
+    FloatPlayer player(track);
+    track.addEvent(FloatEvent(5, .1f));
+    track.addEvent(FloatEvent(15, .1f));
+    player.getNextMessage(20);
+    player.getNextMessage(20);
+    REQUIRE_THROWS_AS(player.getNextMessage(20), noNewMessageException);
+}
+
+TEST_CASE("The track player throws a runtime exception when there are no next messages at a certain time", "[TrackPlayer]") {
+    FloatTrack track;
+    FloatPlayer player(track);
+    track.addEvent(FloatEvent(5, .1f));
+    track.addEvent(FloatEvent(15, .1f));
+    player.getNextMessage(10);
+    REQUIRE_THROWS_AS(player.getNextMessage(10), noNewMessageException);
+}
+
+TEST_CASE("The track player has a method which indicates if there are still messages at a time", "[TrackPlayer]") {
+    FloatTrack track;
+    FloatPlayer player(track);
+    track.addEvent(FloatEvent(5, .1f));
+    track.addEvent(FloatEvent(15, .1f));
+    REQUIRE(player.hasMessages(10));
+    player.getNextMessage(10);
+    REQUIRE(! player.hasMessages(10));
+}
+
+TEST_CASE("The track player has a seek method that sets the position to a certain time", "[TrackPlayer]") {
+    FloatTrack track;
+    FloatPlayer player(track);
+    track.addEvent(FloatEvent(5, .0f));
+    track.addEvent(FloatEvent(15, .1f));
+    player.seek(15);
+    REQUIRE(player.getNextMessage(15) == .1f);
+}
+
+TEST_CASE("The seek method supports seeking beyond the upper boundary of the track", "[TrackPlayer]") {
+    FloatTrack track;
+    FloatPlayer player(track);
+    track.addEvent(FloatEvent(5, .0f));
+    track.addEvent(FloatEvent(15, .1f));
+    player.seek(1000);
+    REQUIRE(! player.hasMessages(1000));
+}
+
+TEST_CASE("The track player has a rewind method that sets the player to 0", "[TrackPlayer]") {
+    FloatTrack track;
+    FloatPlayer player(track);
+    track.addEvent(FloatEvent(5, .0f));
+    track.addEvent(FloatEvent(15, .1f));
+    REQUIRE(player.getNextMessage(15) == .0f);
+    REQUIRE(player.getNextMessage(15) == .1f);
+    player.rewind();
+    REQUIRE(player.getNextMessage(15) == .0f);
+}
+
+TEST_CASE("The track player has a timeLength method that returns the length in milliseconds", "[TrackPlayer]") {
+    FloatTrack track;
+    FloatPlayer player(track);
+    track.addEvent(FloatEvent(200, .0f));
+    uint64_t length = player.timeLength();
+    REQUIRE(length == 200);
+}
